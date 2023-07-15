8c2e01a148445ccebeeab7ef66abe3c833921595
Ekta Kumari <imt_2020032@iiitm.ac.in>
2023-07-15 14:22:31 +0530

    env add

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 export const fetchNews = () => async dispatch => {
-  const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=f5da6ff82e8249f49ea39f08a6b58aca');
+    const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+        params: {
+          country: 'us',
+        //   apiKey: 'f5da6ff82e8249f49ea39f08a6b58aca'
+          apiKey: process.env.REACT_APP_KEY
+        }
+      });
   
   dispatch({ type: 'FETCH_NEWS', payload: response.data.articles });
 };
